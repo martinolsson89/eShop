d541a4ad1c733e83ba041adbaa77bd9ab921de10
martinolsson89 <115090350+martinolsson89@users.noreply.github.com>
2024-02-20 15:58:22 +0100

    added UI.Storage

---
+++ eShop.UI.Storage/Services/SessionStorage.cs
@@ -0,0 +1,19 @@
+﻿using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+
+namespace eShop.UI.Storage.Services;
+
+public class SessionStorage(ISessionStorageService sessionStorage) : IStorageService
+{
+    public async Task<T> GetAsync<T>(string key) =>
+        await sessionStorage.GetItemAsync<T>(key);
+    
+
+    public async Task SetAsync<T>(string key, T value) =>
+        await sessionStorage.SetItemAsync(key, value);
+    
+
+    public async Task RemoveAsync(string key) =>
+        await sessionStorage.RemoveItemAsync(key);
+    
+}
